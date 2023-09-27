00aa9e4c05c2075157fe655b01ec6781fd9b7242
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-27 10:37:59 +0530

    commit - variables

---
+++ 02-variables/index.js
@@ -0,0 +1,8 @@
+console.log("This contains code JS")
+var a = 52 // a contains 67
+console.log(a)
+
+a = "akash"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7
