aff729f78a02bca560afd77ae3e26272bc33cb48
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-27 11:26:00 +0530

    commit - chapter5

---
+++ 05-practiseset/index.js
@@ -0,0 +1,30 @@
+// Chapter 1 - Q1
+let a = "John"
+let b = 6
+console.log(a + b)
+
+// Chapter 1 - Q2
+console.log(typeof (a+b))
+
+// Chapter 1 - Q3
+const a1 = {
+  name: "Harry",
+  section: 1,
+  isPrincipal: false
+}
+// a1 = 45
+// a1 = {}
+
+// Chapter 1 - Q4
+a1['friend'] = "ravi"  
+a1['name'] = "kush"  
+console.log(a1)
+
+// Chapter 1 - Q5
+const dict = {
+  appreciate: "recognize yourself.",
+  ataraxia: "depression , frustration",
+  YUP: "work, especially hard work."
+}
+
+console.log(dict['YUP'])
