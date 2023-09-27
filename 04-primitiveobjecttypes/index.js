343340d89ce86bad5fad59e7761c790f1827b1b7
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-27 11:19:42 +0530

    commit - primitivetype

---
+++ 04-primitiveobjecttypes/index.js
@@ -0,0 +1,20 @@
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("789") + BigInt("5")
+let e = "John"
+let f = Symbol("I am boy")
+let g
+
+console.log(a, b, c , d, e, f, g)
+console.log(typeof c)
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  "John": true,
+  "Abhram": false,
+  "RRR": 67,
+  "Yash": undefined
+}
+console.log(item["RRR"])
