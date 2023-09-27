4cfac3d519b3812dbdfaf999e23fd8ed47006fad
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-27 15:21:33 +0530

    commit - strings

---
+++ 13-introstrings/index.js
@@ -0,0 +1,21 @@
+let name = "Akash"
+// console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend = 'KKK'
+console.log(friend)
+// let myfriend = "Shubh' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "TAN"
+let boy2 = "HON"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = "App\"le"
+console.log(fruit)
