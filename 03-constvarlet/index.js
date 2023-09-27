a2f4c68057df7e7693b6093a1b618c8b4490ed17
Akash-Traveller <akash.arkvk@gmail.com>
2023-09-27 10:47:04 +0530

    commit - constvarlet

---
+++ 03-constvarlet/index.js
@@ -0,0 +1,16 @@
+console.log("JavaScript tutorial 3: var, let and const")
+
+let b = "Akash";
+const author = "Kumar"
+// author = 5 // Throws an error because constant cannot be changed
+
+b = 4
+const harry = 0;
+
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
